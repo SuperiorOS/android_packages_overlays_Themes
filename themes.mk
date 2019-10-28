5ca3a7b6e694d8de913ddaedb781fe9c25065ed5
El Dainosor <eldainosor@gmail.com>
2019-10-28 15:26:07 -0400

    Initial ThemeStub
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>
    Change-Id: I65d261c705acd83f46544c6b3d2ec94729775894

--- themes.mk
+++ themes.mk
@@ -11,3 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+
+# Overlay ThemePicker to use our ThemeStub
+DEVICE_PACKAGE_OVERLAYS += \
+    packages/overlays/Themes/overlays
+
+PRODUCT_PACKAGES += \
+   AexThemeStub
