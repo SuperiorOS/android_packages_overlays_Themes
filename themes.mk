309845af951feec315459dff7b38543e648a7f73
Arghya Chanda <arghyac35@gmail.com>
2020-01-15 14:16:58 +0530

    Move all font related stuffs here[1/3]
      * Also remove unused fonts
    
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    Change-Id: I69c61bd6401b513b4745c835cb47e30fb8908e52

--- themes.mk
+++ themes.mk
@@ -45,3 +45,18 @@ PRODUCT_PACKAGES += \
 	RedAccent \
 	TealAccent \
 	YellowAccent
+
+# Fonts
+LOCAL_PATH := packages/overlays/Themes/fonts
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/ttf,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
+
+#Font Overalys
+PRODUCT_PACKAGES += \
+	FontArbutusSourceOverlay \
+    FontArvoLatoOverlay \
+    FontGoogleSansOverlay \
+	FontNotoSerifSourceOverlay \
+	FontRubikRubikOverlay
