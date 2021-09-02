7139d23de14e6ad4b676539021b905383e06d2b9
sb6596 <shubhamprince111@gmail.com>
2021-09-02 11:29:33 +0000

    Themes: Add more UIStyle overlays
     * AubergineTheme, BakedGreenTheme, ChocoXTheme, MaterialOceanTheme - inspired from ColorBucket (ArrowOS)
     * Also add Aubergine accent
    
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Change-Id: If8b51f407c040ac0ce653ac723da2f0cbf870d75

--- themes.mk
+++ themes.mk
@@ -15,6 +15,7 @@
 # Accents
 PRODUCT_PACKAGES += \
     AccentColorAmberOverlay \
+    AccentColorAubergineOverlay \
     AccentColorBlueOverlay \
     AccentColorBlueGreyOverlay \
     AccentColorBrownOverlay \
@@ -78,9 +79,13 @@ PRODUCT_PACKAGES += \
 
 # Themes
 PRODUCT_PACKAGES += \
+    AndroidAubergineThemeOverlay \
+    AndroidBakedGreenThemeOverlay \
     AndroidBlackThemeOverlay \
+    AndroidChocoXThemeOverlay \
     AndroidElegantThemeOverlay \
-    AndroidExtendedThemeOverlay
+    AndroidExtendedThemeOverlay \
+    AndroidMaterialOceanThemeOverlay
 
 # Rounded Styles
 PRODUCT_PACKAGES += \
