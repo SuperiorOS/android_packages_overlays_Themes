58de0099407459fa4499e67c1cde72a04b4304de
SagarMakhar <sagarmakhar@gmail.com>
2024-01-01 23:05:29 +0800

    themes: Black theme [3/3]
    vendor: theme: BlackTheme: Turn more color components to black
    
    Cherry-picked from:
    * [1] yaap/vendor_themes@60fbe1f
    * [2] yaap/vendor_themes@fa0d31d
    
    Change-Id: Ifbbf658c4b1d1e871d1a4420255cadbeca44339c
    
    Change-Id: I362c75a9a9456bb4d7ffe12072b1b2ef1ea49dac
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- themes.mk
+++ themes.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Black Theme
+PRODUCT_PACKAGES += \
+    BlackThemeOverlay
+
 # Build some fonts
 PRODUCT_PACKAGES += \
     ArbutusSlab-Regular.ttf \
